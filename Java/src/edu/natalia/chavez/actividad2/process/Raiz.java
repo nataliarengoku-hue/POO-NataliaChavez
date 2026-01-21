639ea3482f5b9794ca5bd0c2866989fd6af509ec
Natalia Meredid Teloxa Chavez <nataliarengoku@gmail.com>
2026-01-21 08:48:12 -0600

    Actualizacion de la actividad 2 Parte 1

---
+++ Java/src/edu/natalia/chavez/actividad2/process/Raiz.java
@@ -0,0 +1,31 @@
+package edu.natalia.chavez.actividad2.process;
+
+/**
+ * En esta clase se calcula la raiz n-esima de un numero
+ */
+public class Raiz {
+    /**
+     * Calcula la raiz entera de op1 con indice op2.
+     * @param op1 radicando
+     * @param op2 indice de la raiz
+     * @return la raiz entera
+     */
+    public static int realizarOperacion(int op1, int op2){
+        if (op2 == 0) throw new ArithmeticException("El indice cero no esta permitido");
+        int raiz = 0;
+        while(true){
+            int potencia = 1;
+            for(int i=0; i<op2; i++){
+                potencia = Multiplcacion.realizarOperacion(potencia, raiz);
+            }
+            
+            if(potencia > op1){
+                return Resta.realizarOperacion(raiz, 1);
+            }
+            if(potencia == op1){
+                return raiz;
+            }
+            raiz = Suma.realizarOperacion(raiz, 1);
+        }
+    }
+}
